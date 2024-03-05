3c581bd3a56a80ea45d46e9c852221114c9803ca
MartinM25 <45079129+MartinM25@users.noreply.github.com>
2024-03-05 20:16:15 +0200

    update font.tsx
    fixing deployment error

--- app/font/font.tsx
+++ app/font/font.tsx
@@ -2,23 +2,23 @@ import localFont from "next/font/local";
 
 export const kodeMono = localFont({
   src: [
-    // {
-    //   path: "kodeMono-Bold.woff2",
-    //   weight: "700",
-    //   style: "normal"
-    // },
     {
-      path: "kodeMono-Medium.woff2",
+      path: "KodeMono-Bold.woff2",
+      weight: "700",
+      style: "normal"
+    },
+    {
+      path: "KodeMono-Medium.woff2",
       weight: "500",
       style: "normal"
     },
     {
-      path: "kodeMono-Regular.woff2",
+      path: "KodeMono-Regular.woff2",
       weight: "400",
       style: "normal"
     },
     {
-      path: "kodeMono-SemiBold.woff2",
+      path: "KodeMono-SemiBold.woff2",
       weight: "600",
       style: "normal"
     }
