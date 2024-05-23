fc4ec1095700de1a7e3a57b2802f5c99b008f83c
Martin Manjoro <Martin.manjoro25@gmail.com>
2024-05-23 14:00:49 +0200

    Update button.tsx

--- components/button.tsx
+++ components/button.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button"
 
 export function ResumeButton() {
   return (
-    <Link href="https://drive.google.com/file/d/195E10JOwYAuHdq785eW-8aXT82ZPEanC/view">  
+    <Link href="https://drive.google.com/file/d/1EV0F-Mk15BCbctAxH2uiiVmoUU5k0EZa/view?usp=sharing">  
       <Button className="justify-center bg-blue hover:bg-blue/90 font-KodeMono">
         Resume 
       </Button>
